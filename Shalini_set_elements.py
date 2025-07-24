76d3637feaeea87780506268dc7a6eb3712d1d59
BabyShaliniChandran <Baby.Chandran@Coforge.com>
2025-07-24 17:14:08 +0530

    Update Shalini_set_elements.py

--- Shalini_set_elements.py
+++ Shalini_set_elements.py
@@ -2,5 +2,5 @@ A = {1, 2, 3, 4, 5}
 B = {3, 4, 5, 6, 7}  
 C = {5, 6, 7, 8, 9}
 D = {1, 3, 7, 9, 10}
-
-print((A&B)^(A&C)^(A&D)^(B&C)^(B&D)^(C&D)^(A&B&C)^(A&B&D)^(B&C&D))
+result=(A&B)^(A&C)^(A&D)^(B&C)^(B&D)^(C&D)^(A&B&C)^(A&B&D)^(B&C&D)
+print(f"Elements in exactly 2 sets: {result}")
